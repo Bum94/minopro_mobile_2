a0b591b071a1381fd5a94a683595165a81cd3cb9
Bum94 <65011212094@msu.ac.th>
2024-10-18 00:57:29 +0700

    Up

---
+++ lib/page/rider/TakingOrders.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TakingOrders extends StatefulWidget {
+  const TakingOrders({super.key});
+
+  @override
+  State<TakingOrders> createState() => _nameState();
+}
+
+class _nameState extends State<TakingOrders> {
+  @override
+  Widget build(BuildContext context) {
+    return TakingOrders();
+  }
+}
