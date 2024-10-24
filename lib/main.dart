594a50055a34cf8bcf5e2e24d4c959f62176c328
Bum94 <65011212094@msu.ac.th>
2024-10-24 19:26:22 +0700

    updatelastui

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_minipro2/firebase_options.dart';
 import 'package:flutter_minipro2/page/SysHome.dart';
 import 'package:flutter_minipro2/page/rider/Home.dart';
-import 'package:flutter_minipro2/page/user/AddProduct.dart';
-import 'package:flutter_minipro2/page/user/AwaitJob.dart';
-import 'package:flutter_minipro2/page/user/ProductList.dart';
-import 'package:flutter_minipro2/page/user/ProductSend.dart';
-import 'package:flutter_minipro2/page/user/SearchUser.dart';
+import 'package:flutter_minipro2/page/rider/register_rider.dart';
+import 'package:flutter_minipro2/page/user/register_user.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart'; // Import ครั้งเดียวก็เพียงพอ
 
-void main() async{
-   WidgetsFlutterBinding.ensureInitialized();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   FirebaseFirestore.instance.settings = const Settings(
     persistenceEnabled: true,
   );
-
 
   runApp(const MyApp());
 }
@@ -35,7 +31,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePageRider(), // ตรวจสอบว่าหน้านี้มีการสร้างไว้อย่างถูกต้อง
+      home:
+          const RegisterRiderPage(), // ตรวจสอบว่าหน้านี้มีการสร้างไว้อย่างถูกต้อง
     );
   }
 }
